30a5f569a4373eba4c61abafd99bfe3f65e0a1bb
yeeun <jovn268@naver.com>
2025-05-17 00:44:56 +0900

    JSX문법의 특징과 UI구현법(스타일)

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,3 +1,21 @@
+import "./Main.css"
 export default function Main(){
-    return <h1>메인 컴포넌트 이지롱!</h1>
+const user={
+    name:"정예은",
+    isLogin:true
+}
+    // return <>
+    // {/* 객체를 그대로 사용하면 안되니깐 (JSX주의사항)
+    // 객체의 점표기법을 사용해서 접근함.
+    // */}
+    // {user.isLogin?<div>로그아웃</div>:<div>로그인</div>}
+    // </>
+
+// 조건문 사용하기 
+if(user.isLogin){
+    return<>
+    <div className="logout">로그아웃2</div></>
+}return<>
+<div>로그인</div></>
+
 }
