1cda4b476d7a8b1498ddb9aead5645a1b0e6d96b
yeeun-kor <jovm268@naver.com>
2025-05-16 17:08:56 +0900

    1️⃣Node.js 모듈시스템 - Common JS 사용하기기

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 //- Common Js모듈 시스템 내장함수인 require함수를 사용해서 export한 key값을 불러올 수 있음.
 const moduleData = require("./math");
-console.log(moduleData);
+console.log(moduleData.add(3, 5));
+console.log(moduleData.sub(3, 5));
+
+//- 구조분해 할당 사용(export의 key값을 가지고 옴)
+const { add, sub } = require("./math");
+console.log(add(1, 2));
+console.log(sub(1, 2));
