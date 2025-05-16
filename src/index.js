17cfede6fcd367b083b964091e281e164a2ea0cf
yeeun-kor <jovm268@naver.com>
2025-05-16 17:16:05 +0900

    2️⃣Node.js 모듈시스템 - ES모듈시스템 사용하기(1)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-//- Common Js모듈 시스템 내장함수인 require함수를 사용해서 export한 key값을 불러올 수 있음.
-const moduleData = require("./math");
-console.log(moduleData.add(3, 5));
-console.log(moduleData.sub(3, 5));
+// //- Common Js모듈 시스템 내장함수인 require함수를 사용해서 export한 key값을 불러올 수 있음.
+// const moduleData = require("./math");
+// console.log(moduleData.add(3, 5));
+// console.log(moduleData.sub(3, 5));
 
-//- 구조분해 할당 사용(export의 key값을 가지고 옴)
-const { add, sub } = require("./math");
-console.log(add(1, 2));
-console.log(sub(1, 2));
+// //- 구조분해 할당 사용(export의 key값을 가지고 옴)
+// const { add, sub } = require("./math");
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+
+//- ES모듈 시스템에서 값 받아오기
+//import 객체명 from 경로(*경로는 꼭 확장자명 .js까지 붙여줘야지 찾을 수있음.)
+import { add, sub } from "./math.js";
+console.log(add(5, 1));
+console.log(sub(5, 1));
--- src/math.js
+++ src/math.js
@@ -6,6 +6,10 @@ function add(a, b) {
 function sub(a, b) {
   return a - b;
 }
-//- Common Js모듈 시스템 사용해서 함수 내보내기
-module.exports = { add, sub };
-//보내려는 key값의 이름과, 그에 해당하는 함수명이 동일한 경우 1개만 적어도 무방하다 -> 알아서 찾아줌.
+// //- Common Js모듈 시스템 사용해서 함수 내보내기
+// module.exports = { add, sub };
+// //보내려는 key값의 이름과, 그에 해당하는 함수명이 동일한 경우 1개만 적어도 무방하다 -> 알아서 찾아줌.
+
+//- ES 모듈시스템 사용하기
+//export키워드 뒤에 객체를 리터럴로 생성
+export { add, sub };
