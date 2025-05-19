79bf6fcd2055d8d6b46649cbdae557d984fa130a
yeeun-kor <jovm268@naver.com>
2025-05-19 16:45:04 +0900

    state란?상태함수란?

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import "./App.css";
-import Button from "./components/Button";
-import Header from "./components/Header";
 
 function App() {
+  //state생성
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
   return (
     <>
-      <Button text={"메일"} color={"blue"} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <Header></Header>
-      </Button>
+      {/* 전구 */}
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "ON" ? "OFF" : "ON");
+          }}
+        >
+          {light === "ON" ? "전구 끄기" : "전구 켜기"}
+        </button>
+      </div>
+      {/* 버튼count */}
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </>
   );
 }
