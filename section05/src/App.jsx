592cc732f46eca144a065ace883a1b16c7cabcbe
yeeun-kor <jovm268@naver.com>
2025-05-19 17:10:41 +0900

    state와 props 를 넘겼을때 발생되는 상황 - 컴포넌트 분리화(state 별도로 묶어주기)

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from "react";
 import "./App.css";
+import Bulb from "./components/Bulb";
+import Counter from "./components/Countet";
 
 function App() {
-  //state생성
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
   return (
     <>
       {/* 전구 */}
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === "ON" ? "OFF" : "ON");
-          }}
-        >
-          {light === "ON" ? "전구 끄기" : "전구 켜기"}
-        </button>
-      </div>
+      <Bulb></Bulb>
       {/* 버튼count */}
-      <div>
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
+      <Counter></Counter>
     </>
   );
 }
