07c6654426622ca5c97eb767cbd13efbb4b41ced
yeeun <jovn268@naver.com>
2025-05-19 11:09:53 +0900

    이벤트 핸들러

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,7 +1,13 @@
-const Button = ({ text, color, children }) => {
+const Button = ({ text, color = "black", children }) => {
+  // 이벤트 객체 
+  // 이벤트 핸들러 함수가 호출되면, 무조건 이벤트 객체가 생성됨.
+  const onClickButton = (e) => {
+    console.log(e)
+    console.log(text)
+  }
   return (
-    <button style={{ color: color }}>
-      {text}- {color}
+    <button onClick={onClickButton} style={{ color: color }}>
+      {text}- {color.toUpperCase()}
       {children}
     </button>
   );
