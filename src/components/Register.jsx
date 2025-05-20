1a05a18520e44feec31af016d646547e1c9e717b
yeeun <jovn268@naver.com>
2025-05-20 10:00:59 +0900

    폴더정리

---
+++ src/components/Register.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+//회원가입 폼 만들기
+//1. 이름
+//2. 생년월일
+//3. 국적
+//4. 자기소개
+
+export default function Register() {
+  //* 통합 상태 저장
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio:""
+})
+
+  // * 통합 이벤트 핸들러
+  const onChange = (e) => {
+    setInput({...input,[e.target.name]:e.target.value}
+  )}
+
+  return (
+    <div>
+      <div>
+        {/* 1. 이름 */}
+        <input
+        name="name"  value={input.name} placeholder="이름" onChange={onChange} />
+        입력값 : {input.name}
+      </div>
+      <div>
+        {/* 2. 생년월일 */}
+        <input
+        name="birth"  value={input.birth} type="date" onChange={onChange} />
+        입력값 : {input.birth}
+      </div>
+      <div>
+        {/* 3. 국적 선택 */}
+        <select
+        name="country"  onChange={onChange} value={input.country}>
+          <option value="없음">선택</option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="ek">영국</option>
+        </select>
+        국적 확인 : {input.country}
+      </div>
+      <div>
+        {/* 자기소개 */}
+        <textarea
+          name="bio"
+          onChange={onChange}
+          placeholder="자기소개"
+          value={input.bio}
+        ></textarea>
+        자기소개 내용 : {input.bio}
+      </div>
+    </div>
+  );
+}
