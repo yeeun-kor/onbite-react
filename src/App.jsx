ddb72f8bc6600207bf4da04981fcedce1796cbe5
yeeun-kor <jovm268@naver.com>
2025-05-22 17:43:09 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Controller from "./components/Controller";
 import Viewer from "./components/Viewer";
 
 function App() {
   const [count, setCount] = useState(0);
-  //Controller로 props보낼것임
-  //매개변수인 value 는 각 버튼에 해당하는 값을 받을 것임.
-  // 예를들어 -1버튼이면 그 버튼의 value는 -1값이다.
+  const [input, setInput] = useState("");
+
+  //useEffect 호출
+  // useEffect(() => {
+  //   console.log(`count값은 ? : ${count} / input의 값은 ? : ${input}`);
+  // }, [count, input]);
+
   const onClickButton = (value) => {
     setCount(count + value);
+    console.log(count);
   };
 
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </section>
       <section>
         <Viewer count={count}></Viewer>
       </section>
